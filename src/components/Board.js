ab4a8f3d8a8830c166b52506020b02c81edc4bb2
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-08-27 21:05:32 +0700

    fill the blank with code

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,10 +5,23 @@ export default function Board({ squares, handleClick }) {
   return (
     <div className="board">
       <div>
-        <div className="board-row">"Your code here"</div>
-        <div className="board-row">"Your code here"</div>
-        <div className="board-row">"Your code here"</div>
+        <div className="board-row">
+          <Square value={squares[0]} handleClick={() => handleClick(0)} /*Your code here*/ />
+          <Square value={squares[1]} handleClick={() => handleClick(1)} /*Your code here*/ />
+          <Square value={squares[2]} handleClick={() => handleClick(2)} /*Your code here*/ />
+        </div>
+        <div className="board-row">
+          <Square value={squares[3]} handleClick={() => handleClick(3)} />
+          <Square value={squares[4]} handleClick={() => handleClick(4)} />
+          <Square value={squares[5]} handleClick={() => handleClick(5)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[6]} handleClick={() => handleClick(6)} />
+          <Square value={squares[7]} handleClick={() => handleClick(7)} />
+          <Square value={squares[8]} handleClick={() => handleClick(8)} />
+        </div>
       </div>
     </div>
   );
 }
+
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Square({"Your code here"}) {
+function Square({ value, handleClick/*Your code here*/ }) {
   return (
-    <button className={"square"} onClick={"Your code here"}>
-      "Your code here"
+    <button className={"square"} onClick={handleClick}>
+      {value}
     </button>
   );
 }
